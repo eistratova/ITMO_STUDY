e9bc746edbc441949ec945d59cb529931fbdc6f7
Catherine <catherine.istratova@gmail.com>
2022-11-09 10:17:57 +0300

    Индивид задание 34

--- vrem_ryad_rolling_window.py
+++ vrem_ryad_rolling_window.py
@@ -1,3 +1,5 @@
+# Считаем скользящее среднее (окно 3 и 7) SMA
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,32 +19,16 @@ smothing = series['Y'].to_frame()
  
 # calculating simple moving average
 # using .rolling(window).mean() ,
-# with window size = 30
+# with window size = 3
 smothing['SMA3'] = series['Y'].rolling(3).mean()
- 
-# removing all the NULL values using
+
+smothing['SMA3_w7'] = series['Y'].rolling(7).mean()
+# removing all the NULL values using -  в нашем задании их удалять не нужно
 # dropna() method
-smothing.dropna(inplace=True)
+#smothing.dropna(inplace=True)
  
 # printing Dataframe
 print(smothing)
-
-'''
-
-# Пишем функцию сглаживания
-def SMA_smoothing(series, width):
-    result = []
-    k = (width - 1) // 2
-    for index in range(k, len(series) - k):
-        result.append(np.sum(series[index - k : index + k + 1])/width) 
-    return result
-
-
-smoothed = pd.DataFrame(SMA_smoothing(series[1].to_list(),3))
-smoothed.index += 1
-print('Сглаженный временной ряд-3',smoothed.head())
-
-'''
 # Построим графики исходного и сглаженного рядов
 
 plt.figure(figsize=(20, 8))
@@ -52,11 +38,16 @@ plt.xlabel('Time step')
 plt.ylabel('Y')
 #plt.show()
 
+
+
 # ВЫведем значение 5
 print('ВЫведем значение сглаженное', 5)
 print(smothing.loc[[5]])
 
-'''
-#smoothed = series.rolling(window=7).mean()
-#smoothed
-'''
+plt.figure(figsize=(20, 8))
+plt.plot('Y', data = smothing)
+plt.plot('SMA3_w7', data = smothing)
+plt.xlabel('Time step')
+plt.ylabel('Y')
+plt.show()
+
