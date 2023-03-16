e8166631a44ea9fd2f5ea1ff42f7a8df712beb38
Catherine <catherine.istratova@gmail.com>
2023-03-16 16:04:35 +0300

    IZ_26_Hypotezis_IceCream+datas

--- Zadachi_ITMO_teorver_statistica/IZ_STAT_ICE_CREAM.py
+++ Zadachi_ITMO_teorver_statistica/IZ_STAT_ICE_CREAM.py
@@ -1,18 +1,21 @@
+'''
+Производитель мороженого утверждает, что вес одного эскимо составляет 102  грамм. 
+Из очередной поставки в магазин случайно выбрано и взвешено 8 эскимо. 
+Считается, что масса эскимо имеет нормальное распределение. 
+Справедливо ли заявление продавца, если уровень значимости 0.10.
+Введите оценку среднеквадратического отклонения S_0:
+Введите выборочное среднее :
+Введите модуль функции отклонения |P(x)| :
+Ответьте на вопрос, верна ли гипотеза:
+'''
+
 from calendar import c
 import pandas as pd
-from scipy.stats import t
+from scipy.stats import t, norm
 from matplotlib import pyplot as plt
 import numpy as np
-'''
-df = pd.read_csv('/Users/ekaterina/data4repos/hypothesis_2.csv')
-#print(df)
 
-df.index += 1
-print(df.head(20))
-
-print(t.ppf(1-0.05/2, 99))
-'''
-df_ice_cream = pd.read_csv('/Users/ekaterina/data4repos/Hypothesis_1_7_ICE_CREAM.csv', header=None)
+df_ice_cream = pd.read_csv('/Users/ekaterina/github_repos/ITMO_STUDY/Zadachi_ITMO_teorver_statistica/Hypothesis_1_7.csv', header=None)
 df_ice_cream.index += 1
 
 print(df_ice_cream)
@@ -28,9 +31,9 @@ S = (np.var(df_ice_cream[0], ddof=1))**0.5
 print("Дисперсия несмещенная", S)
 
 
-P_vib = (n**0.5)*(MEAN-a)/(S**0.5)
+Po_X = (n**0.5)*(MEAN-a)/(S)
 
-print ("P_vib",P_vib)
-print("C", t.ppf(1-(e/2), 7))
+print ("Po_X",Po_X)
+print("C", norm.ppf(1-e/2))
 
-print("If P(x)< c Гипотезу принимаем, если > - отвергаем ")
+print("If Po_X < C Гипотезу принимаем, если > - отвергаем. Cогласно нашим результатам гипотеза что вес одного эскимо составляет 102  грамм не отвергаеся ")
