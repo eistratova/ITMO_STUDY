8db3f0f493dada0e2c0f7368de787913ebb868e5
Catherine <catherine.istratova@gmail.com>
2023-03-16 15:18:16 +0300

    Опрос_гипотеза_Tподг_самолета26 мин+данные

--- Zadachi_ITMO_teorver_statistica/HYPOTEZIS_ZADACHI.py
+++ Zadachi_ITMO_teorver_statistica/HYPOTEZIS_ZADACHI.py
@@ -1,9 +1,48 @@
+'''
+Представитель аэропорта на встрече с руководством авиакомпании заявил, 
+что среднее время подготовки самолета к вылету составляет а_0=26 минут. 
+В авиакомпании решили проанализировать, не ошибается ли представитель аэропорта
+и решили рассмотреть последние т=10 обслуживаний. Считается, что время обслуживания 
+распределено нормально, причем дисперсия известна и равна S^2=6. 
+Справедливо ли заявление представителя аэропорта, если уровень значимости e = 0.01. 
+Ответьте на вопрос, верна ли гипотеза?
+
+Введите выборочное среднее, Введите модуль функции отклонения Po(X) :
+'''
+
 from calendar import c
 import pandas as pd
-from scipy.stats import t
+from scipy.stats import norm
 from matplotlib import pyplot as plt
 import numpy as np
+
+df_aeroport = pd.read_csv('/Users/ekaterina/github_repos/ITMO_STUDY/Zadachi_ITMO_teorver_statistica/hypothesis_4.csv', header=None)
+print(df_aeroport)
+
+df_aeroport = df_aeroport[0].round(3)
+df_aeroport = df_aeroport.reset_index()
+df_aeroport[0].index += 1
+
+n = 10  # объем выборки
+aº = 26 # количество минут
+e = 0.01 # уровень значимости
+S = 6 # сигма в квадрате
+
+
+X_m = df_aeroport[0].mean()
+print('Cреднее X_m',df_aeroport[0].mean())
+
+# Формула для рассчета Ро
+# Po = (n**0.5)*((mean*aº)/S)
+Po_vib = (n**0.5)*((X_m-aº)/(S**0.5))
+
+print ("P_vib",Po_vib)
+print("C", norm.ppf(1-e/2), 10)
+
+print("If Po(x)< c Гипотезу принимаем, если > - отвергаем: Po_vib, C ",Po_vib, norm.ppf(1-e/2), 10 )
+print("If Po(x)< c Гипотезу то гипотеза о том, что среднее время обслуживания составляет 26 минут, не находит подтверждения-отвергаем, т к Ро > C")
 '''
+какие-то след задачи
 df = pd.read_csv('/Users/ekaterina/data4repos/hypothesis_2.csv')
 #print(df)
 
@@ -12,47 +51,6 @@ print(df.head(20))
 
 print(t.ppf(1-0.05/2, 99))
 '''
-df_aeroport = pd.read_csv('/Users/ekaterina/data4repos/hypothesis_4aeroport.csv', header=None)
-print(df_aeroport)
-
-df_aeroport = df_aeroport[0].round(3)
-df_aeroport = df_aeroport.reset_index()
-df_aeroport[0].index += 1
-#mean = np.mean(df_aeroport)
-#print(" Cреднее значение",mean)
-
-
-n = 10  # объем выборки
-aº = 26 # количество минут
-e = 0.01 # уровень значимости
-S = 6 # сигма в квадрате
-
-#P = (n*0.5)*((mean*aº)/S)
-#print ("P",P)
-MEAN = df_aeroport[0].mean()
-print('MEAN',df_aeroport[0].mean())
-
-quant_0_5 = np.quantile(df_aeroport[0], 0.5)
-P_руками = 3.162*((29.321-26)/2.449)
-
-P_vib = (n**0.5)*((MEAN-aº)/(S**0.5))
-print ("RUCAMI", P_руками)
-print ("P_vib",P_vib)
-print("C", t.ppf(1-(0.01/2), 9))
-print("корень из n", n**0.5)
-# If P(x)< c
-print("If P(x)< c Гипотезу принимаем, если > - отвергаем ")
-
-
-
-#print("Дисперсия смещенная", np.var(df_aeroport[1]))
-
-#print("Дисперсия несмещенная", np.var(df_new[1], ddof=1))
-
-
-#print("квантиль середина 0,5", np.quantile(df_new[1], 0.5))
-#print("квантиль 0,25", np.quantile(df_new[1], 0.25))
-
 '''
 df_X = pd.read_csv('/Users/ekaterina/data4repos/Hipotezis_1_det_sad/Hyp_2_1_X.csv')
 #print("X", df_X)
